543401402514bc8de302a060b05ca3dfe01fd968
Ahnaf Al Nafis <ahnafalnafis@gmail.com>
2023-06-24 09:52:07 +0600

    Added a naive login and registration feature

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,36 @@
 int main(void) {
   Database db = Database("./database.json");
 
-  /* *
-   * TODO:
-   * Finally, implement the login and registration process!
-   * */
+  char answer;
+  std::string email, password;
 
+  // Confirmation:
+  std::cout << "Do you have an account? [y/n]: ";
+  std::cin >> answer;
+
+  std::cout << "Email: ";
+  std::cin >> email;
+
+  std::cout << "Password: ";
+  std::cin >> password;
+
+  User user = User("", email, password);
+
+  if (answer == 'y') {
+    if (!db.exists(user)) {
+      std::cout << "Sorry, this account does not exist.\n";
+    }
+  } else {
+    std::string username;
+
+    std::cout << "Give a username: ";
+    std::cin >> username;
+    user.setUserName(username);
+
+    db.createUser(user);
+  }
+
+  std::cout << "Successfully logged in.\n";
   db.save();
   return 0;
 }
