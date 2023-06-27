505ccae2626abd5ddc0e2708c072b5309eafd468
Ahnaf Al Nafis <ahnafalnafis@gmail.com>
2023-06-27 17:15:05 +0600

    [chore]: Added header guards and formatted code

--- src/cli/connection.hpp
+++ src/cli/connection.hpp
@@ -1,43 +1,56 @@
+#ifndef CONNECTION_HPP
+#define CONNECTION_HPP
+
 #include <string>
 
 // Full list of databases supported by this project
 enum DB_TYPES {
 
-    MongoDB
-
-}
-
-// This struct will need to change once we formally define what user data will be stored in the DB
-struct UserData {
-
-    std::string identifier;
-    std::string password;
+  MongoDB
 
 };
 
-// This class provides functions for each individual DB connection class to implement
+/* *
+ * This struct will need to change once we formally define what user data will
+ * be stored in the DB
+ * */
+struct UserData {
+  std::string identifier;
+  std::string password;
+};
+
+/* *
+ * This class provides functions for each individual DB connection class to
+ * implement
+ * */
 class Connection {
+ public:
+  /* *
+   * Return types of these functions are subject to change, int is just most
+   * basic choice
+   * */
 
-    // Return types of these functions are subject to change, int is just most basic choice
-    public: 
+  // Connection functions
+  virtual int openConnection() = 0;
+  virtual int closeConnection() = 0;
 
-        // Connection functions
-        virtual int openConnection () = 0;
-        virtual int closeConnection () = 0;
+  // Functions for managing underlying user data structures
+  virtual int initializeAuthStructureInDB() = 0;
+  virtual int destroyAuthStructureInDB() = 0;
 
-        // Functions for managing underlying user data structures
-        virtual int initializeAuthStructureInDB () = 0;
-        virtual int destroyAuthStructureInDB () = 0;
+  // Functions for managing user data in the DB
+  virtual int queryByUniqueIdentifier(std::string identifier) = 0;
+  virtual int addUserToDB(UserData userData);
+  virtual int deleteUserFromDB(std::string identifier) = 0;
+  virtual int wipeAllUserDataFromDB() = 0;
 
-        // Functions for managing user data in the DB
-        virtual int queryByUniqueIdentifier ( std::string identifier ) = 0;
-        virtual int addUserToDB ( UserData userData  );
-        virtual int deleteUserFromDB ( std::string identifier ) = 0;
-        virtual int wipeAllUserDataFromDB () = 0;
+ private:
+  /* *
+   * Make sure the connection object knows what kind of database it's
+   * connected to
+   * */
 
-    private:
+  DB_TYPES connectionType;
+};
 
-        // Make sure the connection object knows what kind of database it's connected to
-        DB_TYPES connectionType;
-
-}
+#endif
