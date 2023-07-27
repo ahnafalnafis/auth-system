2024f39bdf609de455188afe7fc271133cce8990
Ahnaf Al Nafis <ahnafalnafis@gmail.com>
2023-07-27 18:16:41 +0600

    🧹 chore: Public status codes

---
+++ include/status.hpp
@@ -0,0 +1,14 @@
+/**
+ * Copyright (C) 2023 Ahnaf Al Nafis
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef INCLUDE_STATUS_HPP_
+#define INCLUDE_STATUS_HPP_
+
+#include <cstdint>
+
+// Status codes:
+enum Status : uint16_t { SUCCESS, FAIL = 101 };
+
+#endif  // INCLUDE_STATUS_HPP_
