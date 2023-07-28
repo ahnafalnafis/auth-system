32872fef054133517279efd55339808f4f884824
Ahnaf Al Nafis <ahnafalnafis@gmail.com>
2023-07-28 23:21:56 +0600

    🎨 style: Format examples/postgres/mock_data.sql

--- examples/postgres/mock_data.sql
+++ examples/postgres/mock_data.sql
@@ -1,10 +1,89 @@
-insert into mock_users (email, password, date_created) values ('jdowden0@nih.gov', 'uR0+@QA"nAt', '2022-08-25 09:33:04');
-insert into mock_users (email, password, date_created) values ('gpaynton1@jimdo.com', 'eR7=)6@.DzC.\', '2023-02-17 18:04:16');
-insert into mock_users (email, password, date_created) values ('kdevoiels2@cocolog-nifty.com', 'aQ1`apwb=C=W', '2022-12-08 20:16:05');
-insert into mock_users (email, password, date_created) values ('afenton3@intel.com', 'dH0"XK4=', '2023-06-15 04:48:49');
-insert into mock_users (email, password, date_created) values ('cmartel4@abc.net.au', 'pZ1`.64YNo', '2023-05-17 09:16:16');
-insert into mock_users (email, password, date_created) values ('nhalsworth5@ucsd.edu', 'iH4"P_03', '2023-06-26 10:43:52');
-insert into mock_users (email, password, date_created) values ('fworlidge6@yahoo.co.jp', 'kV0>lM|%a', '2023-04-23 08:41:21');
-insert into mock_users (email, password, date_created) values ('vplewman7@shinystat.com', 'dE5,p_.`E''BAUw', '2022-10-13 02:42:21');
-insert into mock_users (email, password, date_created) values ('gwyldbore8@whitehouse.gov', 'iL4(gWD&p&lnRWe', '2023-05-01 22:08:25');
-insert into mock_users (email, password, date_created) values ('jhryskiewicz9@mlb.com', 'aP3#IwmM', '2022-12-25 11:16:41');
+insert into
+  mock_users (email, password, date_created)
+values
+  (
+    'jdowden0@nih.gov',
+    'uR0+@QA"nAt',
+    '2022-08-25 09:33:04'
+  );
+
+insert into
+  mock_users (email, password, date_created)
+values
+  (
+    'gpaynton1@jimdo.com',
+    'eR7=)6@.DzC.',
+    '2023-02-17 18:04:16'
+  );
+
+insert into
+  mock_users (email, password, date_created)
+values
+  (
+    'kdevoiels2@cocolog-nifty.com',
+    'aQ1`apwb=C=W',
+    '2022-12-08 20:16:05'
+  );
+
+insert into
+  mock_users (email, password, date_created)
+values
+  (
+    'afenton3@intel.com',
+    'dH0"XK4=',
+    '2023-06-15 04:48:49'
+  );
+
+insert into
+  mock_users (email, password, date_created)
+values
+  (
+    'cmartel4@abc.net.au',
+    'pZ1`.64YNo',
+    '2023-05-17 09:16:16'
+  );
+
+insert into
+  mock_users (email, password, date_created)
+values
+  (
+    'nhalsworth5@ucsd.edu',
+    'iH4"P_03',
+    '2023-06-26 10:43:52'
+  );
+
+insert into
+  mock_users (email, password, date_created)
+values
+  (
+    'fworlidge6@yahoo.co.jp',
+    'kV0>lM|%a',
+    '2023-04-23 08:41:21'
+  );
+
+insert into
+  mock_users (email, password, date_created)
+values
+  (
+    'vplewman7@shinystat.com',
+    'dE5,p_.`E''BAUw',
+    '2022-10-13 02:42:21'
+  );
+
+insert into
+  mock_users (email, password, date_created)
+values
+  (
+    'gwyldbore8@whitehouse.gov',
+    'iL4(gWD&p&lnRWe',
+    '2023-05-01 22:08:25'
+  );
+
+insert into
+  mock_users (email, password, date_created)
+values
+  (
+    'jhryskiewicz9@mlb.com',
+    'aP3#IwmM',
+    '2022-12-25 11:16:41'
+  );
