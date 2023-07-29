63a71571b3350961ece1ea27037c4e5aaae82ad7
Ahnaf Al Nafis <ahnafalnafis@gmail.com>
2023-07-29 22:15:24 +0600

    🧹 chore: Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,10 @@
 #include "nlohmann/json.hpp"
 
 int main(int argc, char *argv[]) {
-  auto ok = auth::Register({{"email", "ahnafalnafis@gmail.com"},
-                            {"password", "very_strong_password"},
-                            {"date_created", "2003-10-22 12:47:40"}});
+  auto status = auth::Register({{"email", "ahnafalnafis@gmail.com"},
+                                {"password", "very_strong_password"},
+                                {"date_created", "2003-10-22 12:47:40"}});
 
-  std::cout << ok << std::endl;
-  return SUCCESS;
+  std::cout << status << std::endl;
+  return 0;
 }
