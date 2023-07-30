ec2120e833ea686bdcd6c68208cb356c67b4cf21
Ahnaf Al Nafis <ahnafalnafis@gmail.com>
2023-07-31 00:11:12 +0600

    🧹 chore: Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,18 @@
  * SPDX-License-Identifier: MIT
  */
 
-#include <fstream>
 #include <iostream>
 
-#include "auth.hpp"
-#include "connection.hpp"
-#include "nlohmann/json.hpp"
+#include "auth.hpp"  // auth::Register
 
 int main(int argc, char *argv[]) {
-  auto status = auth::Register({{"email", "ahnafalnafis@gmail.com"},
-                                {"password", "very_strong_password"},
-                                {"date_created", "2003-10-22 12:47:40"}});
+  const auto &response =
+    auth::Register(/* user_data */
+                   {{"email", "ahnafalnafis@gmail.com"},
+                    {"password", "very_strong_password"},
+                    {"date_created", "2003-10-22 12:47:40"}});
 
-  std::cout << status << std::endl;
+  std::cout << response.status_code << std::endl;
+
   return 0;
 }
