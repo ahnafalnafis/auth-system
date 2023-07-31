55cff9c4159c27fd02b708796accaf58323a0b00
Ahnaf Al Nafis <ahnafalnafis@gmail.com>
2023-07-31 13:03:15 +0600

    🧹 chore: Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,16 @@ int main(int argc, char *argv[]) {
                     {"password", "very_strong_password"},
                     {"date_created", "2003-10-22 12:47:40"}});
 
+  /**
+   * List of status codes:
+   *   0 = SUCCESS
+   * 101 = FAIL
+   * 102 = PROGRAM_FAILURE
+   * 103 = SERVER_FAULT
+   * 104 = DATABASE_FAULT
+   * 105 = QUERY_FAULT
+   * 106 = UNCONFIGURED_DATABASE
+   */
   std::cout << response.status_code << std::endl;
 
   return 0;
